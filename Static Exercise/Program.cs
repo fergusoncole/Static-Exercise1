564a32b10a96bec57a3d0fd9efd321a9499e283d
Cole Ferguson <fergusoncole98@outlook.com>
2022-07-15 14:56:53 -0500

    Initial Commit

---
+++ Static Exercise/Program.cs
@@ -0,0 +1,9 @@
+﻿using Static_Exercise;
+using System;
+Console.WriteLine("Just a couple examples of Fahrenheit to Celsius:");
+Console.WriteLine($"100° Fahrenheit is {TempConverter.FahrenheitToCelsius(100)}° Celsius.");
+Console.WriteLine($"0° Fahrenheit is {TempConverter.FahrenheitToCelsius(0)}° Celsius.");
+Console.WriteLine();//Space
+Console.WriteLine("Now a couple of examples from Celsius to Fahrenheit:");
+Console.WriteLine($"100° Celsius is {TempConverter.CelsiusToFahrenheit(100)}° Fahrenheit.");
+Console.WriteLine($"0° Celsius is {TempConverter.CelsiusToFahrenheit(0)}° Fahrenheit.");
---
+++ Static Exercise/TempConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_Exercise
+{
+    internal class TempConverter
+    {
+        public static double FahrenheitToCelsius(double x)
+        {
+            return (x - 32) * 5 / 9;
+        }
+
+        public static double CelsiusToFahrenheit(double x)
+        {
+            return (x * 9) / 5 + 32;
+        }
+    }
+}
